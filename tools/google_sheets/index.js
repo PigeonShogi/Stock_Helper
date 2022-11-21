d3c834029553ff9e26dff0abae86015ccd1b18f8
Ckai <ckai.trans@gmail.com>
2022-11-21 17:47:30 +0800

    feat: modify sample code of google sheets api

--- tools/google_sheets/index.js
+++ tools/google_sheets/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const fs = require('fs').promises
 const path = require('path')
 const process = require('process')
@@ -5,7 +6,7 @@ const { authenticate } = require('@google-cloud/local-auth')
 const { google } = require('googleapis')
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -66,26 +67,24 @@ async function authorize () {
 }
 
 /**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * 輸出欲連接資料表的資料
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-async function listMajors (auth) {
+async function getSheet (auth) {
   const sheets = google.sheets({ version: 'v4', auth })
   const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E'
+    spreadsheetId: process.env.SHEET_ID,
+    range: 'Sheet1'
   })
   const rows = res.data.values
   if (!rows || rows.length === 0) {
     console.log('No data found.')
     return
   }
-  console.log('Name, Major:')
-  rows.forEach((row) => {
-    // Print columns A and E, which correspond to indices 0 and 4.
-    console.log(`${row[0]}, ${row[4]}`)
-  })
+  console.log('表單欄位名稱：')
+  console.log(rows)
 }
 
-authorize().then(listMajors).catch(console.error)
+authorize()
+  .then(getSheet)
+  .catch(console.error)
